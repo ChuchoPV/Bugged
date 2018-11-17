bbbc789b68bb777388ef61e001ec350035a46456
OmarChuchin <A01378844@itesm.mx>
2018-11-17 13:14:34 -0600

    Proyectil Hank "funcionando" (no se destruye, esa linea esta comentada)

--- core/src/com/jpv/Bugged/Niveles/Sprites/Items/ProyectilHank.java
+++ core/src/com/jpv/Bugged/Niveles/Sprites/Items/ProyectilHank.java
@@ -7,11 +7,12 @@ import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.jpv.Bugged.Niveles.LevelManager;
 import com.jpv.Bugged.Niveles.Screens.PlayScreen;
+import com.jpv.Bugged.Niveles.Sprites.Character;
 
-public class ProyectilHank extends Item{
+public class ProyectilHank extends Item {
     private boolean first;
     private boolean fliped;
-    private float velocity=1;
+    private float velocity = 1;
 
     public ProyectilHank(PlayScreen screen, float x, float y, boolean fliped) {
         super(screen, x, y);
@@ -19,7 +20,7 @@ public class ProyectilHank extends Item{
         this.fliped = fliped;
         b2body.setGravityScale(0);
         TextureRegion region = new TextureRegion(screen.getAtlas().findRegion("projectile_salt"), 0, 0, 50, 70); //Hank_Shoot
-        region.flip(true,false);
+        region.flip(this.fliped, false);
         setRegion(region);
 
     }
@@ -27,7 +28,7 @@ public class ProyectilHank extends Item{
     @Override
     public void defineItem() {
         BodyDef bdef = new BodyDef();
-        bdef.position.set(getX()+1,getY());
+        bdef.position.set(getX() + 1, getY());
         bdef.type = BodyDef.BodyType.DynamicBody;
         b2body = world.createBody(bdef);
 
@@ -57,25 +58,39 @@ public class ProyectilHank extends Item{
     @Override
     public void update(float dt) {
         super.update(dt);
-        setPosition(b2body.getPosition().x -getWidth() / 2.5f , b2body.getPosition().y - getHeight() / 1.3f);
-        this.b2body.setActive(true);
+        setPosition(b2body.getPosition().x - getWidth() / 2.5f, b2body.getPosition().y - getHeight() / 1.3f);
+        b2body.setActive(true);
+        /*
+        if ((screen.getPlayer().currentState == Character.State.SHOT)) {
+            //this.b2body.applyLinearImpulse(new Vector2(0.1f,0),b2body.getWorldCenter(),true);
+            this.b2body.setLinearVelocity(.2f, 0);
+        }*/
 
-        if(!fliped){
-            if(this.b2body.getLinearVelocity().x<velocity*10){
-                this.b2body.applyLinearImpulse(new Vector2(velocity,0),this.b2body.getWorldCenter(),true);
+        if (!this.fliped) {
+            if (this.b2body.getLinearVelocity().x < velocity * 10) {
+                this.b2body.applyLinearImpulse(new Vector2(velocity, 0), this.b2body.getWorldCenter(), true);
+            } else {
+                this.b2body.setLinearVelocity(velocity * 10, 0);
             }
-            else{
-                this.b2body.setLinearVelocity(velocity*10,0);
+        } else {
+            if (this.b2body.getLinearVelocity().x > -velocity * 10) {
+                this.b2body.applyLinearImpulse(new Vector2(-velocity, 0), this.b2body.getWorldCenter(), true);
+            } else {
+                this.b2body.setLinearVelocity(-velocity * 10, 0);
             }
+
+        /*
+        else if(b2body.getLinearVelocity().x <= 0.2f){
+            this.b2body.applyLinearImpulse(new Vector2(-0.1f,0),b2body.getWorldCenter(),true);
         }
-        else{
-            if(this.b2body.getLinearVelocity().x>-velocity*10){
-                this.b2body.applyLinearImpulse(new Vector2(-velocity,0),this.b2body.getWorldCenter(),true);
-            }
-            else{
-                this.b2body.setLinearVelocity(-velocity*10,0);
-            }
+        if (this.b2body.getLinearVelocity().x<=-4f)
+            this.b2body.setLinearVelocity(-4f,0);
+
+        if(first) {
+            super.reverseVelocity(fliped, false);
+            first = false;
+        }*/
         }
+
     }
-
 }
