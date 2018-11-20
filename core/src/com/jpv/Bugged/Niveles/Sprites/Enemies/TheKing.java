85c60f31d6194554db5db3c6a5e1963cbf7c3744
OmarChuchin <A01378844@itesm.mx>
2018-11-20 16:58:32 -0600

    TheKing 1.5.1 (flip)

--- core/src/com/jpv/Bugged/Niveles/Sprites/Enemies/TheKing.java
+++ core/src/com/jpv/Bugged/Niveles/Sprites/Enemies/TheKing.java
@@ -185,13 +185,13 @@ public class TheKing extends Enemy {
             //SI NADA DE LO DEMAS ESTA PASANDO, Y NO ESTOY MUERTO ESTOY QUIETO (IDLE)
             if(!destroyed){
                 region = (TextureRegion) idle.getKeyFrame(stateTimer,true);
-                if(super.toFlip()){
+                if(!super.toFlip()){
                 //if (screen.getPlayer().b2body.getPosition().x > b2body.getPosition().x){
                     if (region.isFlipX())
                         region.flip(true, false);
                     isRight = false;
                 }//else if (screen.getPlayer().b2body.getPosition().x < b2body.getPosition().x && !isRight) {
-                else if(!super.toFlip()){
+                else if(super.toFlip()){
                     if (!region.isFlipX())
                         region.flip(true, false);
                     isRight = false;
